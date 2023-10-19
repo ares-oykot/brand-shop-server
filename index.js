ce492bdc94d8529b25c7bb4121812fbceaa760be
ares-oykot <soykotislam131415@gmail.com>
2023-10-19 21:28:04 +0600

    added delete method for remove cart informatin to the client side and database

--- index.js
+++ index.js
@@ -172,10 +172,16 @@ async function run() {
             res.send(result);
         });
         ///////////////
-        app.get('cart', async (req, res) => {
+        app.get('/cart', async (req, res) => {
             const cursor = cartCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        });
+        ///////////////
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
         })
         //////////////////////////////////////////////
 
